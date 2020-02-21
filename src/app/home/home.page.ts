7db7691bef7c3edb3ce0693d5cfbdab590fffca4
vennelakovuru <60244042+vennelakovuru@users.noreply.github.com>
2020-02-21 17:06:28 -0600

    api calls

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {max} from "rxjs/operators";
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    public classifyData = null;
 
-  constructor() {}
+    constructor(private httpClient: HttpClient) {
+    }
+
+    public result;
+    public movieDetails;
+    public searchMovie;
+
+    getMovieDetails() {
+        this.httpClient.get('https://api.themoviedb.org/3/search/movie?api_key=79d0f381ad0a8b1c3fb900afef69ee6c&query=' + this.searchMovie)
+            .subscribe(data => {
+                this.movieDetails = data;
+            });
+        console.log(this.movieDetails);
+    }
+
+    classifyCheck() {
+        this.httpClient.get('https://cors-anywhere.herokuapp.com/' + 'https://api.uclassify.com/v1/uClassify/ageanalyzer/classify/?readKey=YGzIEYAJW6Q4&text=' + this.movieDetails.results[0].overview)
+            .subscribe(data => {
+                this.classifyData = data;
+                console.log(this.classifyData);
+            });
+    }
+
 
 }
